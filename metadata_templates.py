8de19dd18ab7f3ff93f9b0215c17651062deab67
Terrell Russell <terrellrussell@gmail.com>
2024-03-19 19:56:18 -0400

    factor out json, return values, usage.md

--- metadata_templates.py
+++ metadata_templates.py
@@ -12,74 +12,59 @@ import requests
 
 from genquery import *
 
-def metadata_templates_attach(rule_args, callback, rei):
-    # Attach (logical_path, schema, type)
-    # - Initially, type will just be 'url', and collection only
-    # - Could later be 'irods_schema' and store the id from the new table
-    # - Or 'form' for ManGO wrapper/form information
+MT_NAMESPACE = 'irods::metadata_templates'
 
-    # parameter error checking
-    # - type is part of enum
+def mt_build_json_input(entity_name, entity_type, operation, value, units):
+    json_input = {
+        "entity_name": entity_name,
+        "entity_type": entity_type,
+        "operations": [
+            {
+                "operation": operation,
+                "attribute": MT_NAMESPACE,
+                "value": value,
+                "units": units
+            }
+        ]
+    }
+    return json_input
 
+
+def mt_attach(callback, entity_name, entity_type, value, units):
     # build JSON input for atomic payload
-    logical_path = rule_args[0]
-    schema = rule_args[1]
-    type = rule_args[2]
-
-    json_input = {
-        "entity_name": logical_path,
-        "entity_type": 'collection',
-        "operations": [
-            {
-                "operation": 'add',
-                "attribute": 'irods::metadata_templates',
-                "value": schema,
-                "units": type
-            }
-        ]
-    }
-
+    json_input = mt_build_json_input(entity_name, entity_type, 'add', value, units)
     # call atomic
     result = callback.msi_atomic_apply_metadata_operations(json.dumps(json_input), "")
-
     # check for errors
     ### something something ... result['arguments'][0]
 
 
-def metadata_templates_detach(rule_args, callback, rei):
+def mt_detach(callback, entity_name, entity_type, value, units):
+    # build JSON input for atomic payload
+    json_input = mt_build_json_input(entity_name, entity_type, 'remove', value, units)
+    # call atomic
+    result = callback.msi_atomic_apply_metadata_operations(json.dumps(json_input), "")
+    # check for errors
+    ### something something ... result['arguments'][0]
+
+
+def metadata_templates_collection_attach(rule_args, callback, rei):
+    # Attach (logical_path, schema, type)
+    logical_path = rule_args[0]
+    schema = rule_args[1]
+    type = rule_args[2]
+    result = mt_attach(callback, logical_path, 'collection', schema, type)
+
+
+def metadata_templates_collection_detach(rule_args, callback, rei):
     # Detach (logical_path, schema, type)
-
-    # query to find/match the object
-    # remove the AVU
-    # A: irods::metadata_templates
-    # V: <url>|<filename>|<data_id>/<logical_path>
-    # U: type (url|local|irods) or 'form' too?
-
     logical_path = rule_args[0]
     schema = rule_args[1]
     type = rule_args[2]
-
-    json_input = {
-        "entity_name": logical_path,
-        "entity_type": 'collection',
-        "operations": [
-            {
-                "operation": 'remove',
-                "attribute": 'irods::metadata_templates',
-                "value": schema,
-                "units": type
-            }
-        ]
-    }
-
-    # call atomic
-    result = callback.msi_atomic_apply_metadata_operations(json.dumps(json_input), "")
-
-    # check for errors
-    ### something something ... result['arguments'][0]
+    result = mt_detach(callback, logical_path, 'collection', schema, type)
 
 
-def metadata_templates_gather(rule_args, callback, rei):
+def metadata_templates_collection_gather(rule_args, callback, rei):
     # Export/Collapse/Rasterize/Gather/Dump (logical_path, recursive)
     # - Find all associated schemas and construct effective schema
     # - Recursive would check/gather all parents up to root
@@ -91,25 +76,33 @@ def metadata_templates_gather(rule_args, callback, rei):
     schemas = []
     for schema, thetype in Query(callback,
                         "META_COLL_ATTR_VALUE, META_COLL_ATTR_UNITS",
-                        "COLL_NAME = '{}' and META_COLL_ATTR_NAME = 'irods::metadata_templates'".format(logical_path)):
+                        "COLL_NAME = '{}' and META_COLL_ATTR_NAME = '{}'".format(logical_path, MT_NAMESPACE)):
         callback.writeLine('serverLog','{} {}'.format(thetype, schema))
         if thetype == 'url':
             try:
+                # get the schema content from the location
                 r = requests.get(schema)
-                callback.writeLine('serverLog', r.content)
+#                callback.writeLine('serverLog', r.content)
+                # convert to json object
                 j = json.loads(r.content)
-                callback.writeLine('serverLog', j)
-                # build a composition of these schemas and declare AllOf
+#                callback.writeLine('serverLog', j)
+                # add to schemas array
+                # TODO: perhaps we should just store the string? rather than the json object?
                 schemas.append(j)
             except Exception as e:
 #                callback.writeLine('serverLog', '{}: {}'.format(type(e), e))
                 callback.writeLine('serverLog', '{}'.format(type(e)))
         else:
             callback.writeLine('serverLog', 'Type [{}] Not Supported By Metadata Templates'.format(thetype))
-    combinedschema = {"type": object, "allOf": schemas}
+#    combinedschema = {"type": object, "allOf": schemas}
 #    callback.writeLine('serverLog', type(combinedschema))
+    # can we return anything other than a string?  i was hoping for an array...
+      # do i have to put it into a rule_arg[]?
+    rule_args[2] = repr(schemas)
+#    rule_args[2] = schemas
+#    return schemas
 
-def metadata_templates_validate(rule_args, callback, rei):
+def metadata_templates_collection_validate(rule_args, callback, rei):
     # Validate (logical_path, recursive)
     # - Run gather (above) to build the effective json schema
     # - Get and build json payload with all current AVUs
@@ -120,3 +113,23 @@ def metadata_templates_validate(rule_args, callback, rei):
     recursive = rule_args[1]
 
 
+    # get all metadata on this collection (wait, or are we checking a data object and against the schemas on its parent? hmmm)
+    thedata = {}
+    for a, v, u in Query(callback, # do we need units?  json-schema is just key/value?
+                        "META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE, META_COLL_ATTR_UNITS",
+                        "COLL_NAME = '{}' and META_COLL_ATTR_NAME != '{}'".format(logical_path, MT_NAMESPACE)):
+        themetadata[a] = v # will this stomp on identical a/v combinations, that have different units in the catalog?
+
+    # run metadata through each schema, collecting any errors
+    errors = []
+    for s in schemas:
+        try:
+            jsonschema.validate(themetadata, s)
+        except Exception as e:
+            errors.append(e)
+    # if anything failed, return the errors
+    if errors:
+        callback.writeLine('serverLog', 'SCHEMA ERRORS: {}'.format(repr(errors)))
+        return -2
+    # we did it
+    return 0
--- usage.md
+++ usage.md
@@ -3,11 +3,11 @@ This demonstrates calling the rules, written in Python, but via the iRODS Rule L
 # attach
 
 ```
-$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_attach('*logical_path', '*schema_location', 'url')" '*logical_path=/tempZone/home/rods/thedir%*schema_location=http://example.org' ruleExecOut
-$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_attach('*logical_path', '*schema_location', 'url')" '*logical_path=/tempZone/home/rods/thedir%*schema_location=http://example.org.again' ruleExecOut
-$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_attach('*logical_path', '*schema_location', 'url')" '*logical_path=/tempZone/home/rods/thedir%*schema_location=https://raw.githubusercontent.com/fge/sample-json-schemas/master/geojson/geojson.json' ruleExecOut
-$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_attach('*logical_path', '*schema_location', 'notaurl')" '*logical_path=/tempZone/home/rods/thedir%*schema_location=somethingelse' ruleExecOut
-$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_attach('*logical_path', '*schema_location', 'removeme')" '*logical_path=/tempZone/home/rods/thedir%*schema_location=doesnotexist' ruleExecOut
+$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_collection_attach('*logical_path', '*schema_location', 'url')" '*logical_path=/tempZone/home/rods/thedir%*schema_location=http://example.org' ruleExecOut
+$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_collection_attach('*logical_path', '*schema_location', 'url')" '*logical_path=/tempZone/home/rods/thedir%*schema_location=http://example.org.again' ruleExecOut
+$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_collection_attach('*logical_path', '*schema_location', 'url')" '*logical_path=/tempZone/home/rods/thedir%*schema_location=https://raw.githubusercontent.com/fge/sample-json-schemas/master/geojson/geojson.json' ruleExecOut
+$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_collection_attach('*logical_path', '*schema_location', 'notaurl')" '*logical_path=/tempZone/home/rods/thedir%*schema_location=somethingelse' ruleExecOut
+$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_collection_attach('*logical_path', '*schema_location', 'removeme')" '*logical_path=/tempZone/home/rods/thedir%*schema_location=doesnotexist' ruleExecOut
 ```
 
 ```
@@ -15,7 +15,7 @@ $ imeta ls -C thedir
 AVUs defined for collection /tempZone/home/rods/thedir:
 attribute: irods::metadata_templates
 value: doesnotexist
-units: deleteme
+units: removeme
 ----
 attribute: irods::metadata_templates
 value: http://example.org
@@ -37,7 +37,7 @@ units: notaurl
 # detach
 
 ```
-$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_detach('*logical_path', '*schema_location', 'removeme')" '*logical_path=/tempZone/home/rods/thedir%*schema_location=doesnotexist' ruleExecOut
+$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_collection_detach('*logical_path', '*schema_location', 'removeme')" '*logical_path=/tempZone/home/rods/thedir%*schema_location=doesnotexist' ruleExecOut
 ```
 
 ```
@@ -63,7 +63,12 @@ units: notaurl
 # gather
 
 ```
-$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_gather('*logical_path', *recursive)" '*logical_path=/tempZone/home/rods/thedir%*recursive=0' ruleExecOut
+$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_collection_gather('*logical_path', *recursive)" '*logical_path=/tempZone/home/rods/thedir%*recursive=0' ruleExecOut
+```
+
+with inout to stdout
+```
+$ irule -r irods_rule_engine_plugin-irods_rule_language-instance "metadata_templates_collection_gather(*logical_path, *recursive, *inout);writeLine('stdout',*inout)" '*logical_path=/tempZone/home/rods/thedir%*recursive=0%*inout=""' ruleExecOut
 ```
 
 # validate
